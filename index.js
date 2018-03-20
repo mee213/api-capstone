0d9a32943a195967f1769eeb5f9671e9c313816b
mee213 <evans.marlo@gmail.com>
2018-03-20 17:06:12 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+  console.log('getDataFromApi ran');
+  const settings = {
+    url: YOUTUBE_SEARCH_URL,
+    data: {
+      'maxResults': '25',
+      'part': 'snippet',
+      'q': searchTerm,
+      'type': 'video',
+      'key': 'AIzaSyBhVeDVxN5VgRfxHEIQVeks5m3t-0ob6Qw'
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: callback
+  };
+
+  console.log(settings);
+
+  $.ajax(settings);
+}
+/*
+function renderResult(result) {
+  console.log('renderResult ran');
+  console.log(result);
+  return `
+    <div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/${result.id.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+  `;
+}
+*/
+function renderResult(result) {
+  console.log('renderResult ran');
+  console.log(result);
+  return `
+    <div>
+      <a class="js-result-thumbnail" href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">
+        <img src="${result.snippet.thumbnails.medium.url}" alt="${result.snippet.title}"/>
+        <p>${result.snippet.title}</p>
+      </a> 
+    </div>
+  `;
+}
+
+function displayYouTubeSearchData(data) {
+  console.log('displayYouTubeSearchData ran');
+  const results = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+  console.log('watchSubmit ran');
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    console.log('Submit button clicked');
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    console.log(`The search term is ${query}`);
+    getDataFromApi(query, displayYouTubeSearchData);
+  });
+}
+
+$(watchSubmit);
