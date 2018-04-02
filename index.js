5058d2e8a80a83a315b866d741728908e6d106bd
mee213 <evans.marlo@gmail.com>
2018-04-02 16:49:17 -0500

    initial rough draft of recipe display

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const YUMMLY_SEARCHRECIPES_URL = 'https://api.yummly.com/v1/api/recipes';
-const YUMMLY_GETRECIPE_URL = 'https://api.yummly.com/v1/api/recipe/recipe-id';
+const YUMMLY_GETRECIPE_BASE_URL = 'https://api.yummly.com/v1/api/recipe/';
 
 function getDataFromYummlySearchRecipesApi(searchTerm, callback) {
   console.log('getDataFromYummlySearchRecipesApi ran');
@@ -21,15 +21,14 @@ function getDataFromYummlySearchRecipesApi(searchTerm, callback) {
   $.ajax(settings);
 }
  
-function getDataFromYummlyGetRecipeApi(searchTerm, callback) {
+function getDataFromYummlyGetRecipeApi(recipeId, callback) {
   console.log('getDataFromYummlyGetRecipeApi ran');
   const settings = {
-    url: YUMMLY_GETRECIPE_URL,
+    url: `${YUMMLY_GETRECIPE_BASE_URL}${recipeId}`,
     data: {
       '_app_key': '98d5f50b76c55e907326e264c73e2b06',
       '_app_id': 'fb07a227',
-      'q': searchTerm,
-      'requirePictures': true
+      'q': recipeId
     },
     dataType: 'jsonp',
     type: 'GET',
@@ -41,8 +40,8 @@ function getDataFromYummlyGetRecipeApi(searchTerm, callback) {
   $.ajax(settings);
 }
 
-function renderResult(result) {
-  console.log('renderResult ran');
+function renderSearchResult(result) {
+  console.log('renderSearchResult ran');
   console.log(result);
   return `
     <div>
@@ -54,18 +53,57 @@ function renderResult(result) {
   `;
 }
 
+function renderRecipe(recipe) {
+  console.log('renderRecipe ran');
+  console.log(recipe);
+  const ingredientListHTML = renderIngredientList(recipe);
+  return `
+    <div>
+      <h3>${recipe.attribution.html}</h3>
+      <img src="${recipe.images[0].hostedLargeUrl}"/>
+      <h4>Servings: ${recipe.numberOfServings}</h4>
+      ${ingredientListHTML}
+      <p>Source: <a href="${recipe.source.sourceRecipeUrl}">${recipe.source.sourceDisplayName}</a></p>
+    </div>
+  `;
+}
+
+function renderIngredientList(recipe) {
+  console.log('renderIngredientList ran');
+  let ingrListHTML = "";
+  for (let i = 0; i < recipe.ingredientLines.length; i++) {
+    ingrListHTML += `<p>${recipe.ingredientLines[i]}</p>`;
+  }
+  return ingrListHTML;
+}
+
 function displayYummlySearchData(data) {
   console.log('displayYummlySearchData ran');
   console.log(data);
-  const results = data.matches.map((item, index) => renderResult(item));
+  const results = data.matches.map((item, index) => renderSearchResult(item));
   const attribution = data.attribution.html;
   $('.js-search-results').prop('hidden', false)
   $('.js-search-results').html(attribution)
   $('.js-search-results').append(results);
+
+  // watch for user to click on a recipe & display the recipe
+  $("a").on( "click", function(event) {
+    event.preventDefault();
+    console.log('recipe clicked');
+    const recipeId = $(this).children('p').attr('id');
+    getDataFromYummlyGetRecipeApi(recipeId, displayYummlyRecipe);
+  });
 }
 
-function watchSubmit() {
-  console.log('watchSubmit ran');
+function displayYummlyRecipe(data) {
+  console.log('displayYummlyRecipe ran');
+  console.log(data);
+  const recipeDisplay = renderRecipe(data);
+  $('.js-search-results').html(recipeDisplay);
+}
+
+function watchSubmitSearch() {
+  console.log('watchSubmitSearch ran');
   $('.js-search-form').submit(event => {
     event.preventDefault();
     console.log('Submit button clicked');
@@ -78,4 +116,4 @@ function watchSubmit() {
   });
 }
 
-$(watchSubmit);
+$(watchSubmitSearch);
